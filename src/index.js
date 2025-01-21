55b594d3131a2873ab64c60bb389c4ee9efcf05a
Muhammad Faisal <132137772+FaisalRiaz010@users.noreply.github.com>
2025-01-21 18:49:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ app.use('/thread', thread_routes)
 app.use('/trade', trade_routes)
 
 app.get("/", (req, res) => {
-    res.send("Hello, Welcome to guess meme !");
+    res.send("Hello, Welcome to guess meme!");
 });
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 const notificationRoutes = require('./routes/notification');
